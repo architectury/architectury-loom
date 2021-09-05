49916d3c013cfd4fc5127638e3fc6dc98abc9b46
Juuz <6596629+Juuxel@users.noreply.github.com>
2021-09-05 17:08:15 +0300

    Checkstyle, part II

--- src/main/java/net/fabricmc/loom/util/MappingsProviderVerbose.java
+++ src/main/java/net/fabricmc/loom/util/MappingsProviderVerbose.java
@@ -2,7 +2,6 @@ package net.fabricmc.loom.util;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
