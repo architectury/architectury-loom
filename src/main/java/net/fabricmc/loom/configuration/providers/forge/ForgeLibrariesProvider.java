2c0b232efdcaaa1c888ce547587b808ff2722b3a
Jab125 <67534807+Jab125@users.noreply.github.com>
2023-06-13 22:12:21 +1000

    check for legacy forge correctly

--- src/main/java/net/fabricmc/loom/configuration/providers/forge/ForgeLibrariesProvider.java
+++ src/main/java/net/fabricmc/loom/configuration/providers/forge/ForgeLibrariesProvider.java
@@ -56,7 +56,6 @@ import net.fabricmc.mappingio.tree.MemoryMappingTree;
 public class ForgeLibrariesProvider {
 	private static final String FML_LOADER_GROUP = "net.minecraftforge";
 	private static final String FML_LOADER_NAME = "fmlloader";
-	private static final boolean isLegacyForge = true;
 
 	public static void provide(MappingConfiguration mappingConfiguration, Project project) throws Exception {
 		LoomGradleExtension extension = LoomGradleExtension.get(project);
@@ -64,7 +63,7 @@ public class ForgeLibrariesProvider {
 
 		// Collect all dependencies with possible relocations, such as Mixin.
 		for (JsonElement lib : extension.getForgeUserdevProvider().getJson().get("libraries").getAsJsonArray()) {
-			if (isLegacyForge) {
+			if (extension.isLegacyForge()) {
 				lib = lib.getAsJsonObject().get("name");
 			}
 			String dep = null;
