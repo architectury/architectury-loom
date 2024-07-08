116f8eeefac1ca421eb37894847bdb3c12bb3f88
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-07-08 21:19:45 +1000

    test example mod mixin

---
+++ src/test/resources/projects/forge/1206/src/main/java/com/example/examplemod/mixin/ExampleModMixin.java
@@ -0,0 +1,16 @@
+package com.example.examplemod.mixin;
+
+import net.minecraft.client.Minecraft;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class ExampleModMixin {
+	@Inject(method = "<init>", at = @At("HEAD"))
+	private static void initInject(CallbackInfo info) {
+		System.out.println("Hello from the example mod mixin!");
+	}
+}
