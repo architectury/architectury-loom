81d92d6aefa4dc30e8ed6bb7c8b5732c82a5172f
Jab125 <67534807+Jab125@users.noreply.github.com>
2024-07-11 17:23:02 +1000

    Revert "add multiloader-forge project"
    This reverts commit 9597bec5809934a3d7f43bd9b686bd842605e671.

--- src/test/resources/projects/forge/1206-with-common-project/common/src/main/java/com/example/ExampleMod.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "examplemod";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
