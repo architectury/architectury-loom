83aef832ff507f8941b64df43840a3f705f50bec
秋雨落 <i@rain.cx>
2025-01-27 10:20:42 +0800

    fix: check style.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/dev/architectury/loom/extensions/ModBuildExtensions.java
+++ src/main/java/dev/architectury/loom/extensions/ModBuildExtensions.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
