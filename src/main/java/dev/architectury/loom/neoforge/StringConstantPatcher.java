74794ca7ec858ac18d5b1482af826986f84c2729
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-10-01 11:14:56 +0300

    StringConstantPatcher: Use Constants.ASM_VERSION

--- src/main/java/dev/architectury/loom/neoforge/StringConstantPatcher.java
+++ src/main/java/dev/architectury/loom/neoforge/StringConstantPatcher.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.jetbrains.annotations.Nullable;
 import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
 
 import net.fabricmc.loom.api.mappings.layered.MappingsNamespace;
 import net.fabricmc.loom.util.Constants;
@@ -106,7 +105,7 @@ public final class StringConstantPatcher extends ClassVisitor {
 
 	private final class MethodPatcher extends MethodVisitor {
 		MethodPatcher(MethodVisitor next) {
-			super(Opcodes.ASM9, next);
+			super(Constants.ASM_VERSION, next);
 		}
 
 		@Override
