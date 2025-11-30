ec2faea53c9e5d2fb30bd6d464b6cd25e068afeb
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-11-30 21:49:10 +0200

    PatchProvider: Extract env-specific patch files on demand (#311)

--- src/main/java/dev/architectury/loom/forge/minecraft/MinecraftPatchedProvider.java
+++ src/main/java/dev/architectury/loom/forge/minecraft/MinecraftPatchedProvider.java
@@ -655,8 +655,8 @@ public class MinecraftPatchedProvider {
 	}
 
 	public enum Type {
-		CLIENT_ONLY("client", "client", (patch, userdev) -> patch.clientPatches),
-		SERVER_ONLY("server", "server", (patch, userdev) -> patch.serverPatches),
+		CLIENT_ONLY("client", "client", (patch, userdev) -> patch.extractClientPatches()),
+		SERVER_ONLY("server", "server", (patch, userdev) -> patch.extractServerPatches()),
 		MERGED("merged", "joined", (patch, userdev) -> userdev.getJoinedPatches());
 
 		private final String id;
