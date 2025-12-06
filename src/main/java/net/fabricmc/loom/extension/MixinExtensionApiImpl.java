408b9688555243dcc3c3467dbff855a5f8037fb6
Juuz <6596629+Juuxel@users.noreply.github.com>
2025-12-06 13:42:19 +0200

    Disable the mixin AP on all platforms by default
    Fixes #317.

--- src/main/java/net/fabricmc/loom/extension/MixinExtensionApiImpl.java
+++ src/main/java/net/fabricmc/loom/extension/MixinExtensionApiImpl.java
@@ -56,7 +56,7 @@ public abstract class MixinExtensionApiImpl implements MixinExtensionAPI {
 	public MixinExtensionApiImpl(Project project) {
 		this.project = Objects.requireNonNull(project);
 		this.useMixinAp = project.getObjects().property(Boolean.class)
-				.convention(project.provider(() -> shouldUseMixinApByDefault(project)));
+				.convention(false);
 
 		this.refmapTargetNamespace = project.getObjects().property(String.class)
 				.convention(project.provider(() -> IntermediaryNamespaces.runtimeIntermediary(project)));
@@ -67,10 +67,6 @@ public abstract class MixinExtensionApiImpl implements MixinExtensionAPI {
 
 		this.showMessageTypes = project.getObjects().property(Boolean.class);
 		this.showMessageTypes.convention(false).finalizeValueOnRead();
-	}
-
-	private static boolean shouldUseMixinApByDefault(Project project) {
-		return LoomGradleExtension.get(project).isForge() && !LoomGradleExtension.get(project).getForgeProvider().usesMojangAtRuntime();
 	}
 
 	protected final PatternSet add0(SourceSet sourceSet, String refmapName) {
