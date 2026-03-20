f2df8c28509654d02db66b7e16b47fa20b2d18e3
shedaniel <daniel@shedaniel.me>
2026-03-21 06:06:50 +0900

    Change plugin id, and update nightly version to build properly

--- src/test/groovy/net/fabricmc/loom/test/LoomTestConstants.groovy
+++ src/test/groovy/net/fabricmc/loom/test/LoomTestConstants.groovy
@@ -42,6 +42,7 @@ class LoomTestConstants {
 		PRE_RELEASE_GRADLE
 	] : [
 		DEFAULT_GRADLE,
+		LoomTestVersions.GRADLE_LATEST.version()
 	]).shuffled().toArray()
 
 	public static final File TEST_DIR = new File("./.gradle/test-files")
