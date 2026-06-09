847455dde21d2a5ddb03ad2d04bcd1a21dc8565e
Juuz <6596629+Juuxel@users.noreply.github.com>
2026-06-09 15:24:20 +0300

    Fix code style

--- src/main/kotlin/net/fabricmc/loom/kotlin/remapping/KotlinMetadataRemappingClassVisitor.kt
+++ src/main/kotlin/net/fabricmc/loom/kotlin/remapping/KotlinMetadataRemappingClassVisitor.kt
@@ -25,7 +25,11 @@
 package net.fabricmc.loom.kotlin.remapping
 
 import org.jetbrains.annotations.VisibleForTesting
-import org.objectweb.asm.*
+import org.objectweb.asm.AnnotationVisitor
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
 import org.objectweb.asm.commons.Remapper
 
 class KotlinMetadataRemappingClassVisitor(
